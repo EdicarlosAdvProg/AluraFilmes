9430ab439e2cc92d44087995a2235ff6ec590003
Edicarlos <edicarlos.mail@gmail.com>
2022-05-31 11:56:38 -0300

    Preparando o ambiente e recuperando tabela de atores

---
+++ Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,27 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Dados {
+    public class AluraFilmesContexto : DbContext {
+
+        public DbSet<Ator> Atores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder
+                .UseSqlServer("Data Source=DESKTOP-H77C52F\\SQLEXPRESS;" +
+                "Initial Catalog=AluraFilmes;" +
+                "User ID=sa;Password=1a5w2s3d;" +
+                "Connect Timeout=30;" +
+                "Encrypt=False;" +
+                "TrustServerCertificate=False;" +
+                "ApplicationIntent=ReadWrite;" +
+                "MultiSubnetFailover=False;");
+        }
+
+    }
+}
---
+++ Alura.Filmes.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Dados;
+using System.Linq;
+
+namespace Alura.Filmes.App {
+    class Program {
+        static void Main(string[] args) {
+
+            using(var context=new AluraFilmesContexto()) {
+                
+                context.LogSQLToConsole();
+               
+                foreach(var item in context.Atores) {
+                    System.Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
